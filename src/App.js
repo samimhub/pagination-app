819231b392db69a7ef8894e8d05cc321b6beb1da
samim <aktarsamim529@gmail.com>
2024-06-01 03:02:42 +0530

    pagination commite

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from 'axios';
+
 
 function App() {
+  const [todos,setTodos]=useState([]);
+  const [todosPerPage,setTodosPerPage]=useState(5);
+  const [currentPage,setCurrentPage]=useState(1)
+  useEffect(()=>{
+    axios.get('https://dummyjson.com/todos')
+    .then((res)=>setTodos(res.data.todos))
+  },[])
+
+  const numberOfTotalPage= Math.ceil(todos.length/todosPerPage);
+  const pages = [...Array(numberOfTotalPage+1).keys()].slice(1);
+  
+  const indexOfLastTodos=currentPage * todosPerPage;
+  const indexOfFirstTodos=indexOfLastTodos - todosPerPage;
+
+  const visibleTodos=todos.slice(indexOfFirstTodos,indexOfLastTodos)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {
+        visibleTodos.map((todoItem)=>(
+          <p key={todoItem.id}>
+            {todoItem.todo}
+          </p>
+        ))
+      }
+      <span>prev</span>
+      <h1>{
+            pages.map((page)=>
+            <span 
+            key={page}
+            onClick={()=>setCurrentPage(page)}
+            className={`${currentPage ===page} ? 'active':''`}
+            >
+            {`${page} | `}
+            </span>)
+          }</h1>
+          <span>next</span>
     </div>
   );
 }

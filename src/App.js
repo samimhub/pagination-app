4132dee1476cd6283486ea0f72f207100ad64456
samim <aktarsamim529@gmail.com>
2024-06-01 03:27:52 +0530

    last and final commite

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from 'axios';
+import './App.css';
 
 
 function App() {
@@ -19,27 +20,44 @@ function App() {
 
   const visibleTodos=todos.slice(indexOfFirstTodos,indexOfLastTodos)
 
+  const prevPageHandler =()=>{
+    if(currentPage !== 1){
+      setCurrentPage(currentPage - 1);
+    }
+  }
+
+  const nextPageHandler =()=>{
+    if(currentPage !== numberOfTotalPage){
+      setCurrentPage(currentPage + 1);
+    }
+  }
+
   return (
     <div>
+      <select onChange={(e)=>setTodosPerPage(e.target.value)}>
+        <option value='5'>5</option>
+        <option value='15'>15</option>
+        <option value='30'>30</option>
+      </select>
       {
         visibleTodos.map((todoItem)=>(
-          <p key={todoItem.id}>
+          <li key={todoItem.id}>
             {todoItem.todo}
-          </p>
+          </li>
         ))
       }
-      <span>prev</span>
-      <h1>{
+      <button onClick={prevPageHandler}>prev</button>
+      <h3>{
             pages.map((page)=>
             <span 
             key={page}
             onClick={()=>setCurrentPage(page)}
-            className={`${currentPage ===page} ? 'active':''`}
+            className={`${currentPage === page ? 'active':''}`}
             >
             {`${page} | `}
             </span>)
-          }</h1>
-          <span>next</span>
+          }</h3>
+          <button onClick={nextPageHandler}>next</button>
     </div>
   );
 }
